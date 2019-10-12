b0ec61f3a587a6061373354a0fb412a047c5b0ec
yashmayya <yash.mayya@gmail.com>
2019-10-12 17:54:00 +0000

    Added support for posting comments which are stored in memory

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,20 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return render_template("main_page.html")
+comments = []
+
+@app.route('/', methods=['GET','POST'])
+def index():
+
+    if request.method == 'GET':
+        return render_template("main_page.html", comments=comments)
+
+    else:
+        comments.append(request.form["contents"])
+        return redirect(url_for('index'))
 
