c16d5839a156d025262b4ab6e949488291739912
yashmayya <yash.mayya@gmail.com>
2019-10-13 15:27:13 +0000

    Moved users to database

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
 from flask_migrate import Migrate
-from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.security import check_password_hash
 from datetime import datetime
 
 app = Flask(__name__)
@@ -29,10 +29,13 @@ app.secret_key = "dhfadfcnkahsjgnfkashjxskahscnjdf"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class User(UserMixin):
-    def __init__(self, username, password_hash):
-        self.username = username
-        self.password_hash = password_hash
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -40,11 +43,11 @@ class User(UserMixin):
     def get_id(self):
         return self.username
 
-all_users = {"admin":User("admin", generate_password_hash("secret")), "yash":User("admin", generate_password_hash("adsfdgfhgj"))}
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    return all_users.get(user_id)
+    return User.query.filter_by(username=user_id).first()
 
 class Comment(db.Model):
 
@@ -77,11 +80,10 @@ def login():
         return render_template("login_page.html", error=False)
 
     else:
-        username = request.form["username"]
-        if username not in all_users:
+        user = load_user(request.form["username"])
+        if user is None:
             return render_template("login_page.html", error=True)
         else:
-            user = all_users[username]
             if not user.check_password(request.form["password"]):
                 return render_template("login_page.html", error=True)
 
