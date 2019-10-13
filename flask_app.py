4bba2b462f08bf33a9316591c5a9cab4e5ebc438
yashmayya <yash.mayya@gmail.com>
2019-10-13 14:03:39 +0000

    Implemented timestamp functionality

--- flask_app.py
+++ flask_app.py
@@ -52,13 +52,13 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
-
+    posted = db.Column(db.DateTime, default=datetime.now)
 
 @app.route('/', methods=['GET','POST'])
 def index():
 
     if request.method == 'GET':
-        return render_template("main_page.html", comments=Comment.query.all(), timestamp = datetime.now())
+        return render_template("main_page.html", comments=Comment.query.all())
 
     else:
         if not current_user.is_authenticated:
